966bcf3d1568fa7640d2c4a46c0473800098b6ea
stella huayhuaca <luces.huayhuaca@gmail.com>
2017-02-17 09:46:59 -0800

    add fixed indentation.

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -1,40 +1,40 @@
 <?php
-require_once 'src/RepeatCounter.php';
+    require_once 'src/RepeatCounter.php';
 
-class RepeatCounterTest extends PHPUnit_Framework_TestCase
+    class RepeatCounterTest extends PHPUnit_Framework_TestCase
 
-{
+    {
 
-  function test_countRepeat()
-  {
-    $RepeatCounter = new RepeatCounter();
-    $input1 = "hi";
-    $input2 = "hi";
+      function test_countRepeat()
+      {
+        $RepeatCounter = new RepeatCounter();
+        $input1 = "hi";
+        $input2 = "hi";
 
-    $result= $RepeatCounter->CountRepeats($input1,$input2);
+        $result= $RepeatCounter->CountRepeats($input1,$input2);
 
-    $this->assertEquals(1, $result);
-  }
-  function test_countRepeats()
-  {
-    $RepeatCounter = new RepeatCounter();
-    $input1 = "hi";
-    $input2 = "hi hi";
+        $this->assertEquals(1, $result);
+      }
+      function test_countRepeats()
+      {
+        $RepeatCounter = new RepeatCounter();
+        $input1 = "hi";
+        $input2 = "hi hi";
 
-    $result= $RepeatCounter->CountRepeats($input1,$input2);
+        $result= $RepeatCounter->CountRepeats($input1,$input2);
 
-    $this->assertEquals(2, $result);
-  }
-  function test_countSentence()
-  {
-    $RepeatCounter = new RepeatCounter();
-    $input1 = "hi";
-    $input2 = "hi hello hi";
+        $this->assertEquals(2, $result);
+      }
+      function test_countSentence()
+      {
+        $RepeatCounter = new RepeatCounter();
+        $input1 = "hi";
+        $input2 = "hi hello hi";
 
-    $result= $RepeatCounter->CountRepeats($input1,$input2);
+        $result= $RepeatCounter->CountRepeats($input1,$input2);
 
-    $this->assertEquals(2, $result);
-  }
-}
+        $this->assertEquals(2, $result);
+      }
+    }
 
- ?>
+?>

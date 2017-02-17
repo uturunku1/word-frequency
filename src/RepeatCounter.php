8d70227b1e1efb84794558fbdb82bc93cffbbdfd
stella huayhuaca <luces.huayhuaca@gmail.com>
2017-02-17 09:18:02 -0800

    add a little style

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -1,20 +1,20 @@
 <?php
-class RepeatCounter
-{
-
-  static function CountRepeats($input1, $input2)
+  class RepeatCounter
   {
-    $words_in_input = explode(" ",$input2);
-    $matches= array();
 
-    foreach ($words_in_input as $word) {
-      if ($input1==$word) {
-        array_push($matches,$word);
+    static function CountRepeats($input1, $input2)
+    {
+      $words_in_input = explode(" ",$input2);
+      $matches= array();
+
+      foreach ($words_in_input as $word) {
+        if ($input1==$word) {
+          array_push($matches,$word);
+        }
       }
+
+      return count($matches);
     }
 
-    return count($matches);
   }
-
-}
  ?>

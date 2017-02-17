506d7de07c0bd8e5d827b4d73dca7b0fb9b291ce
stella huayhuaca <luces.huayhuaca@gmail.com>
2017-02-16 22:01:08 -0800

    add passing tests

---
+++ src/Words.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ */
+class Words
+{
+
+  function CountRepeats($input1, $input2)
+  {
+    $words_in_input = explode(" ",$input2);
+    $matches= array();
+
+    foreach ($words_in_input as $word) {
+      if ($input1==$word) {
+        array_push($matches,$word);
+      }
+    }
+
+    return count($matches);
+  }
+
+}
+ ?>
